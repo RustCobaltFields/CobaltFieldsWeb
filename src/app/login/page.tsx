6a1fc06cb56ec88fe8a1a815e8f42d4f4cb42d71
RashingPro <rashingpro@yandex.ru>
2025-01-05 22:51:26 +0300

    Add empty /login page

---
+++ src/app/login/page.tsx
@@ -0,0 +1,10 @@
+import HeaderModule from "@/module/header/header.module";
+
+export default function Page() {
+    return <>
+        <HeaderModule/>
+        <div className={"pageContainer"}>
+        </div>
+    </>
+
+}
