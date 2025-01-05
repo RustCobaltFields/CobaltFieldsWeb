540451c7ea6970e7b91a9004ec3ec6c4aeef61be
RashingPro <rashingpro@yandex.ru>
2025-01-05 16:02:24 +0300

    Basic project set up

---
+++ src/app/layout.tsx
@@ -0,0 +1,18 @@
+import type { Metadata } from "next";
+import "./globals.css";
+import React from "react";
+
+export const metadata: Metadata = {
+    title: "Cobalt Fields",
+    description: "High quality Rust servers",
+};
+
+export default function RootLayout({children}: Readonly<{children: React.ReactNode;}>) {
+    return (
+        <html lang="en">
+            <body className={`rootContainer antialiased`}>
+                {children}
+            </body>
+        </html>
+  );
+}
