f521c3f229e30624cc8790d9adfda1ea41b9c09c
RashingPro <rashingpro@yandex.ru>
2025-01-10 21:28:24 +0300

    Remove deprecated dynamic_title module

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,6 @@
 import "@/module/header/header.module"
 import HeaderModule from "@/module/header/header.module";
 import "./page.css"
-// import DynamicTitle from "./dynamic_title";
 import TypeWriter from "@/module/type_writer/type_writer.module";
 
 const typeWriterStrings = [
@@ -22,14 +21,13 @@ export default async function Page() {
             <div className={"serverTitle"}>
                 Cobalt Fields
             </div>
-            {/*<DynamicTitle/>*/}
             <div className="titleContainer">
                 <span className="titleStaticPart">На нашем сервере </span>
                 <TypeWriter
                     strings={typeWriterStrings}
                     delayAfterDelete={500}
                     delayAfterPrint={500}
-                    delayBetweenSymbol={75}
+                    delayBetweenSymbol={60}
                     wrapperClassName={"titleDynamicPart"}
                 />
             </div>
